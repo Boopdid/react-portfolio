8fdedf9813b07fb68cbcd4bc9a01a28a31e5d557
Peter Vrutneski <pvrutneski@gmail.com>
2022-08-01 15:37:34 -0600

    Use hashrouter

--- src/App.js
+++ src/App.js
@@ -7,27 +7,29 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basname='/react-portfolio/'>
-      <div className='flex flex-col min-h-screen bg-prussian-blue mx-auto px-2 sm:px-6 lg:px-8'>
-        <Header>
-          <Navigation />
-        </Header>
-        <div className='max-w-7xl mx-auto mb-auto'>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/#/about' element={<About />} />
-            <Route exact path='/#/projects' element={<Projects />} />
-            <Route exact path='/#/contact-me' element={<Contact />} />
-            <Route path='*' element={<div></div>} />
-          </Routes>
+    <React.StrictMode>
+      <Router basname='/react-portfolio/'>
+        <div className='flex flex-col min-h-screen bg-prussian-blue mx-auto px-2 sm:px-6 lg:px-8'>
+          <Header>
+            <Navigation />
+          </Header>
+          <div className='max-w-7xl mx-auto mb-auto'>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/about' element={<About />} />
+              <Route exact path='/projects' element={<Projects />} />
+              <Route exact path='/contact-me' element={<Contact />} />
+              <Route exact path='*' element={<div></div>} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </React.StrictMode>
   );
 }
 export default App;
